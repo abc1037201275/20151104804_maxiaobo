d0714aaec106be69c0909be90312a8541747faed
abc1037201275 <44023156+abc1037201275@users.noreply.github.com>
2019-03-18 22:42:57 +0800

    20190318
    提交论文初稿

---
+++ Tour/src/com/nmg/service/XianluService.java
@@ -0,0 +1,26 @@
+package com.nmg.service;
+
+import java.util.List;
+
+import com.nmg.entity.Xianlu;
+
+public interface XianluService {
+	//插入新纪录
+	public void insertXianluBean(Xianlu bean);
+	
+	//根据用户id删除纪录
+	public void deleteXianluBean(Xianlu bean);
+	
+	//根据用户id更新纪录
+	public void updateXianluBean(Xianlu bean);
+
+	//获取信息列表,带查询功能，start 表示当前页，limit表示每页显示的条数 start=1,lmit=10
+	public List<Xianlu> selectXianluBeanList(final int start,final int limit,final String where);
+	
+	
+	//查询记录的总的条数
+	public long selectXianluBeanCount(final String where);
+	
+	//查询用户信息
+	public Xianlu selectXianluBean(String where);
+}
